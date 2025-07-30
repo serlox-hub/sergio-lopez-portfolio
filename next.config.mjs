baa0d1f59e4f54618444d518eee808b4df9c30a0
Serlox <sergiolopezolmo@gmail.com>
2025-07-30 09:14:17 +0200

    Add extra config por pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,13 @@ const withMDX = mdx({
   options: {},
 });
 
+const repo = "sergio-lopez-portfolio";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   sassOptions: {
